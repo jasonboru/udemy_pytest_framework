688d6e04c912598c73c0dc77b057a60e59c5bb2f
Jason O'Brien <jasonboru@gmail.com>
2023-06-20 15:23:33 -0400

    updated the params on the fixture for getData() to a library instead of tuple. This way the data fields in the operation are more clear. They now show the library keys rather than confusing list indexes.

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -11,20 +11,20 @@ class TestHomePage(BaseClass):
     def test_formSubmission(self, getData):
 
         homePage = HomePage(self.driver)
-        homePage.getName().send_keys(getData[0])
-        homePage.getEmail().send_keys(getData[1])
-        homePage.getPassword().send_keys(getData[2])
+        homePage.getName().send_keys(getData["name"])
+        homePage.getEmail().send_keys(getData["email"])
+        homePage.getPassword().send_keys(getData["password"])
         homePage.getCheckbox().click()
-        self.select_option_from_text(homePage.getGender(), getData[3])
+        self.select_option_from_text(homePage.getGender(), getData["gender"])
         homePage.getEmpStatus().click()
-        homePage.getBirthday().send_keys(getData[4])
+        homePage.getBirthday().send_keys(getData["bday"])
         homePage.submitForm().click()
         alertMessage = homePage.getSuccessMessage().text
         assert ("Success" in alertMessage)
         self.driver.refresh()
 
-    @pytest.fixture(params=[("Jason Boru", "jasonboru@gmail.com", "Password1234", "Male", "01171978"),
-                            ("Nola Pearl", "noladog@gmail.com", "PasswordB@rk", "Female", "09152021")])
+    @pytest.fixture(params=[{"name": "Jason Boru", "email": "jasonboru@gmail.com", "password": "Password1234", "gender": "Male", "bday": "01171978"},
+                            {"name": "Nola Pearl", "email": "noladog@gmail.com", "password": "PasswordB@rk", "gender": "Female", "bday": "09152021"}])
     def getData(self, request):
         return request.param
 
